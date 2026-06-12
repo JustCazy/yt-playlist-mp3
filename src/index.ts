e6ab4534c1f0fbd662517ce64df60975e7be758f
JustCazy <glebvavilov2010@mail.ru>
2026-06-12 11:45:17 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { Download, Playlist } from "./modules";
-
+https://youtube.com/playlist?list=PLp3-KQ922RELlU_q8bmxEMtweK_b8jo0j&si=wcJhuthEIe7dBMvH
 export { Download, Playlist };
+yarn && yarn dev
